a5adeb94657c34bbb083c821827307182a481543
gabbms <gabriel.msousz@gmail.com>
2025-11-22 18:00:25 -0300

    menu principal

--- src/View/MenuPrincipal.java
+++ src/View/MenuPrincipal.java
@@ -2,10 +2,27 @@ package View;
 
 import Exception.*;
 import Model.*;
+import Service.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 
 public class MenuPrincipal {
+    private static Scanner scanner = new Scanner(System.in);
+    private static EventoService eventoService = new EventoService();
+    private static PagamentoService pagamentoService = new PagamentoService();
+    private static PedidoService pedidoService;
+    private static IRelatorio relatorioService;
+
     public static void main(String[] args){
+        pedidoService = new PedidoService(eventoService);
+        relatorioService = new RelatorioService(pagamentoService);
+
+        eventoService.criarEvento("Gala de Tecnologia");
+
+
 
     }
 }
